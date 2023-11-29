06cab0f183bfdb545da0b0f6a44f3a92b58f4eac
Satyamtiwari04 <103598305+Satyamtiwari04@users.noreply.github.com>
2023-11-29 15:59:15 +0530

    Add files via upload

---
+++ controllers/books-controller.js
@@ -0,0 +1,46 @@
+
+const Post = require('../models/postModel');
+
+exports.createPost = async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+
+router.post('/', postController.createPost);
+router.post(9)
+
+module.exports = router;
+const Post = require('../models/postModel');
+
+exports.addCommentToPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    post.comments.push(req.body);
+    const savedPost = await post.save();
+    res.json(savedPost.comments);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+router.post('/:postId/comments', commentController.addCommentToPost);
+
+
+module.exports = router;
+
+
